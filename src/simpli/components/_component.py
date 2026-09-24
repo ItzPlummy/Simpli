9dc04e29e4e971315ccf0772f2b05c49bbbc6c90
Plummy <plummybeatsoff@gmail.com>
2026-09-25 01:12:06 +0200

    Component inheritance now makes a dataclass

--- src/simpli/components/_component.py
+++ src/simpli/components/_component.py
@@ -1,5 +1,21 @@
 from abc import ABC
+from typing import dataclass_transform, Any
+
+from pydantic.dataclasses import dataclass
 
 
+@dataclass_transform(kw_only_default=True)
 class Component(ABC):
-    ...
+    def __init_subclass__(
+            cls,
+            **kwargs: Any,
+    ) -> None:
+        super().__init_subclass__()
+
+        kwargs.update(
+            {
+                "kw_only": True
+            }
+        )
+
+        dataclass(cls, **kwargs)
